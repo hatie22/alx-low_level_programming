0288d90ca7aba0814cb1f873c75c24768bf6c14b
hatie22 <sarponghenrieta50@gmail.com>
2022-06-06 13:30:46 -0700

    The print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: print the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+  char c = 'a';
+
+  while (c <= 'z')
+  {
+    putchar(c);
+    c++;
+  }
+  putchar('\n');
+  return (0);
+}
+
+
