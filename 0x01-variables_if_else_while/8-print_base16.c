05ed6bc64505bca830cd85a1c3e00c26783ec768
hatie22 <sarponghenrieta50@gmail.com>
2022-06-06 15:10:40 -0700

    The 8 print alphabets

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
 /**
  * main - main block
  * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
+ * starting from 0, then followed by a new line.
  * Return: 0
  */
 int main(void)
 {
-  char i = 'z';
+  char c;
+  char d = '0';
 
-  while (i >= 'a')
+  while (d <= '9')
   {
-    putchar(i);
-    i--;
+    putchar(d);
+    d++;
+  }
+
+  for (c = 'a'; c <= 'f'; c++)
+  {
+    putchar(c);
   }
 
   putchar('\n');
   return (0);
 }
+
