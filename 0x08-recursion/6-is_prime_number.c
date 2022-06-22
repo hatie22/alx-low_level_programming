e85b7e169ff9e7d01f6daefe9a63e8e1b59afd71
hatie22 <sarponghenrieta50@gmail.com>
2022-06-22 14:43:33 -0700

    Prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime number
+ * @n: integer
+ * Return: 0
+ */
+
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
+
+/**
+ * divisors - is the number a prime number?
+ * @n: integer
+ * @m: integer
+ * Return: boolean
+ */
+
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
